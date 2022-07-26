e237de1d441ad6c5bd1cbe759b2f2f09919d2ceb
JihadAG <106910410+JihadAG@users.noreply.github.com>
2022-07-26 14:49:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
+//////////////////////////////////////////////login-popup//////////////////////////////////////////////
+let popup = document.getElementById("pop-up");
+function openPopup(){
+  popup.classList.add("open-popup");
+}
+function closePopup(){
+  popup.classList.remove("open-popup");
+}
+////////////////////////////////////////////// Popup-1 //////////////////////////////////////////////
+let popup1 = document.getElementById("pop-up1");
+let popup2 = document.getElementById("pop-up2");
+let popup3 = document.getElementById("pop-up3");
+let popup4 = document.getElementById("pop-up4");
+
+
+function openPopup1(){
+  popup1.classList.add("open-popup");
+}
+function openPopup2(){
+  popup2.classList.add("open-popup1");
+}
+function openPopup3(){
+  popup3.classList.add("open-popup1");
+}
+function openPopup4(){
+  popup4.classList.add("open-popup4");
+}
+function closePopup1(){
+  popup1.classList.remove("open-popup");
+}
+function closePopup2(){
+  popup2.classList.remove("open-popup1");
+}
+function closePopup3(){
+  popup3.classList.remove("open-popup1");
+}
+function closePopup4(){
+  popup4.classList.remove("open-popup4");
+}
